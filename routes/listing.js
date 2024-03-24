6eaf859c09867835c527bce10614d1786fe0c346
swaleh-siddiqui <swalehiffco@gmail.com>
2024-03-24 22:52:54 +0530

    added file type in form

--- routes/listing.js
+++ routes/listing.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload  = multer({dest : "uploads/"});
 
 const Listing = require("../models/listing.js");
 
